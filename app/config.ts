2223749fd56cd64cd892f7a939d67b6756ea696a
nhigocbang <nhigocbang@gmail.com>
2026-02-25 21:08:01 +0700

    Update config.ts

--- app/config.ts
+++ app/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  siteName: process.env.SITE_NAME || "Gia Phả OS",
+  siteName: process.env.SITE_NAME || "HỌ VÕ BÌNH CHÂU",
   exampleEmail: process.env.EXAMPLE_EMAIL || "",
   examplePassword: process.env.EXAMPLE_PASSWORD || "",
 };
